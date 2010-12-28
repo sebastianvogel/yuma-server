7d90c1516b23855078be6df88849d365de607596
Rainer Simon <rainer.simon@ait.ac.at>
2010-12-28 22:02:52 +0100

    Styling for text pages

--- src/main/java/at/ait/dme/yuma/server/gui/doc/Overview.java
+++ src/main/java/at/ait/dme/yuma/server/gui/doc/Overview.java
@@ -1,12 +1,18 @@
 package at.ait.dme.yuma.server.gui.doc;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 
-public class Overview extends WebPage {
+import at.ait.dme.yuma.server.gui.BaseTextPage;
+
+public class Overview extends BaseTextPage {
 	
 	public Overview(final PageParameters parameters) {
+    	super(parameters);
     	
+		add(new Label("title", "YUMA Annotation Server - Documentation - Overview"));		
+		add(new Label("heading", "YUMA Server Documentation"));
+		add(new Label("subheading", "Overview"));
     }
 
 }
