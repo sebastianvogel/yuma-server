19a98c1308653bdc0715a06fd49f44f2d8732b0f
chef <chef@rio>
2011-02-03 17:00:11 +0100

    began adding oac code

--- src/main/java/at/ait/dme/yuma/server/controller/oac/OACFormatHandler.java
+++ src/main/java/at/ait/dme/yuma/server/controller/oac/OACFormatHandler.java
@@ -2,12 +2,18 @@ package at.ait.dme.yuma.server.controller.oac;
 
 import java.util.List;
 
+import at.ait.dme.yuma.server.URIBuilder;
 import at.ait.dme.yuma.server.controller.RDFFormatHandler;
 import at.ait.dme.yuma.server.controller.SerializationLanguage;
 import at.ait.dme.yuma.server.model.Annotation;
 import at.ait.dme.yuma.server.model.AnnotationTree;
+import at.ait.dme.yuma.server.model.tag.SemanticRelation;
+import at.ait.dme.yuma.server.model.tag.SemanticTag;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.DC;
+import com.hp.hpl.jena.vocabulary.RDF;
 
 /**
  * Format handler for OAC RDF(in different serialization
@@ -16,6 +22,12 @@ import com.hp.hpl.jena.rdf.model.Model;
  * @author Rainer Simon
  */
 public class OACFormatHandler extends RDFFormatHandler {
+	
+	private static final String NS_ANNOTATION = "http://annotations.mminf.univie.ac.at/annotation-body#";
+	private static final String NS_OAC = "http://www.openannotation.org/ns/";
+	
+	private Annotation annotation;
+	private Model model;
 	
 	public OACFormatHandler() {
 		super(SerializationLanguage.RDF_XML);
@@ -41,7 +53,71 @@ public class OACFormatHandler extends RDFFormatHandler {
 
 	@Override
 	protected void addRDFResource(Annotation annotation, Model model) {
-		// TODO Auto-generated method stub
+		this.annotation = annotation;
+		this.model = model;
 		
+		model.setNsPrefix("oac", NS_OAC);
+		
+		Resource body = createAnnotationBody();
+		Resource resource = createAnnotationResource(body);
+		
+		
+		annotation.addProperty(m.createProperty(NS_ANNOTATION, "created"), a.getCreated().toString());
+		annotation.addProperty(m.createProperty(NS_ANNOTATION, "modified"), a.getLastModified().toString());
+		
+		if (a.getText() != null)
+			annotation.addProperty(m.createProperty(NS_ANNOTATION, "label"), a.getText());
+		
+		annotation.addProperty(m.createProperty(NS_SCOPE, "scope"), a.getScope().name());
+		
+		if (a.getTags() != null) {
+			for (SemanticTag t : a.getTags()) {
+				SemanticRelation r = t.getRelation();
+				annotation.addProperty(m.createProperty(r.getNamespace(), r.getProperty()), t.getURI().toString());
+			}
+		}
 	}
+	
+	private Resource createAnnotationBody() {
+		Resource ret = model.createResource(
+			URIBuilder.toURI(NS_ANNOTATION).toString());
+		
+		addBodyProperties(ret);
+		return ret;
+	}
+	
+	private void addBodyProperties(Resource body) {
+		addTitle(body);
+		addCreator(body);
+	}
+	
+	private void addTitle(Resource body) {
+		if (annotation.getTitle() != null)
+			body.addProperty(DC.title, annotation.getTitle());
+	}
+	
+	private void addCreator(Resource body) {
+		body.addProperty(DC.creator, annotation.getCreatedBy().getUsername());
+	}
+
+	
+	private Resource createAnnotationResource(Resource body) {
+		Resource ret = model.createResource(
+			URIBuilder.toURI(annotation.getAnnotationID()).toString());		
+		
+		addAnnotationProperties(ret, body);
+		return ret;
+	}
+	
+	private void addAnnotationProperties(Resource annotResource, Resource body)
+	{
+		annotResource.addProperty(RDF.type, model.createProperty(NS_OAC, "Annotation"));
+		annotResource.addProperty(
+			model.createProperty(NS_OAC, "hasBody"), 
+			body.getURI());
+		annotResource.addProperty(
+			model.createProperty(NS_OAC, "hasTarget"), 
+			URIBuilder.toURI(annotation.getObjectUri()).toString());		
+	}
+
 }
