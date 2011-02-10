c147dbfabe6cfa254d70dc1d704cfeed2785dc34
Rainer Simon <rainer.simon@ait.ac.at>
2011-02-10 10:27:15 +0100

    Correct handling of linked resources and SVG fragments in LEMO serialization

--- src/main/java/at/ait/dme/yuma/server/controller/lemo/LEMOFormatHandler.java
+++ src/main/java/at/ait/dme/yuma/server/controller/lemo/LEMOFormatHandler.java
@@ -12,6 +12,7 @@ import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.DC;
 import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
 
 import at.ait.dme.yuma.server.URIBuilder;
 import at.ait.dme.yuma.server.controller.FormatHandler;
@@ -40,7 +41,8 @@ public class LEMOFormatHandler implements FormatHandler {
 	public static final String N_TRIPLE = "N-TRIPLE";
 	public static final String TURTLE = "TURTLE";
 	
-	private static final String NS_ANNOTATION = "http://lemo.mminf.univie.ac.at/annotation-core#";
+	private static final String NS_LEMO_CORE = "http://lemo.mminf.univie.ac.at/annotation-core#";
+	private static final String NS_LEMO_IMAGE = "http://lemo.mminf.univie.ac.at/annotation-image#";
 	private static final String NS_SCOPE = "http://lemo.mminf.univie.ac.at/ann-tel#";
 
 	private String serializationLanguage;
@@ -72,7 +74,7 @@ public class LEMOFormatHandler implements FormatHandler {
 		HashMap<String, Object> properties = new HashMap<String, Object>();
 		
 		ResIterator it =
-			m.listResourcesWithProperty(m.createProperty(NS_ANNOTATION, "annotates"));
+			m.listResourcesWithProperty(m.createProperty(NS_LEMO_CORE, "annotates"));
 		
 		if (it.hasNext()) {
 			Resource a = it.next();
@@ -81,7 +83,7 @@ public class LEMOFormatHandler implements FormatHandler {
 					URIBuilder.toID(a.getURI()));
 			
 			properties.put(MapKeys.ANNOTATION_OBJECT_URI, 
-					a.getProperty(m.createProperty(NS_ANNOTATION, "annotates")).getString());
+					a.getProperty(m.createProperty(NS_LEMO_CORE, "annotates")).getString());
 			
 			properties.put(MapKeys.ANNOTATION_CREATED_BY,
 					new User(a.getProperty(DC.creator).getString()));
@@ -102,7 +104,7 @@ public class LEMOFormatHandler implements FormatHandler {
 					a.getProperty(DC.title).getString());
 			
 			properties.put(MapKeys.ANNOTATION_TEXT,
-					a.getProperty(m.createProperty(NS_ANNOTATION, "label")).getString());	
+					a.getProperty(m.createProperty(NS_LEMO_CORE, "label")).getString());	
 		}
 				
 		return new Annotation(properties);
@@ -134,30 +136,37 @@ public class LEMOFormatHandler implements FormatHandler {
 	}
 	
 	private void addRDFResource(Annotation a, Model m) {
-		m.setNsPrefix("a", NS_ANNOTATION);
+		m.setNsPrefix("a", NS_LEMO_CORE);
+		m.setNsPrefix("image", NS_LEMO_IMAGE);
 		m.setNsPrefix("scope", NS_SCOPE);
 		
 		Resource annotation = m.createResource(URIBuilder.toURI(a.getAnnotationID()).toString());		
 		
-		annotation.addProperty(m.createProperty(NS_ANNOTATION, "annotates"), a.getObjectUri());
-		annotation.addProperty(RDF.type, m.createProperty(NS_ANNOTATION, "Annotation"));
+		annotation.addProperty(m.createProperty(NS_LEMO_CORE, "annotates"), a.getObjectUri());
+		annotation.addProperty(RDF.type, m.createProperty(NS_LEMO_CORE, "Annotation"));
 		
 		if (a.getTitle() != null)
 			annotation.addProperty(DC.title, a.getTitle());
 		
 		annotation.addProperty(DC.creator, a.getCreatedBy().getUsername());
-		annotation.addProperty(m.createProperty(NS_ANNOTATION, "created"), a.getCreated().toString());
-		annotation.addProperty(m.createProperty(NS_ANNOTATION, "modified"), a.getLastModified().toString());
+		annotation.addProperty(m.createProperty(NS_LEMO_CORE, "created"), a.getCreated().toString());
+		annotation.addProperty(m.createProperty(NS_LEMO_CORE, "modified"), a.getLastModified().toString());
 		
 		if (a.getText() != null)
-			annotation.addProperty(m.createProperty(NS_ANNOTATION, "label"), a.getText());
+			annotation.addProperty(m.createProperty(NS_LEMO_CORE, "label"), a.getText());
 		
 		annotation.addProperty(m.createProperty(NS_SCOPE, "scope"), a.getScope().name());
+		annotation.addProperty(m.createProperty(NS_LEMO_IMAGE, "svg-fragment"), m.createLiteral(a.getFragment(), true));
 		
 		if (a.getTags() != null) {
 			for (SemanticTag t : a.getTags()) {
 				SemanticRelation r = t.getRelation();
-				annotation.addProperty(m.createProperty(r.getNamespace(), r.getProperty()), t.getURI().toString());
+				if (r == null) {
+					// Default to rdfs:seeAlso
+					annotation.addProperty(RDFS.seeAlso, t.getURI().toString());
+				} else {
+					annotation.addProperty(m.createProperty(r.getNamespace(), r.getProperty()), t.getURI().toString());
+				}
 			}
 		}
 	}
